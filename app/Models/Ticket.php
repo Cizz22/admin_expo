3fbf47c493750c58530d95cb80caf5c1302a099b
Cizz22 <cisatraa@gmail.com>
2022-07-21 23:13:43 +0700

    fix: 100%

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -7,7 +7,6 @@ use Jenssegers\Mongodb\Eloquent\Model;
 class Ticket extends Model
 {
     protected $connection = 'mongodb';
-    protected $collection = "ticket_dev";
     protected $guarded = [];
 
     public function booking(){
