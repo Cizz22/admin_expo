c343f0a8db54ed5902f20d23541ef4d561d31e16
Cizz22 <cisatraa@gmail.com>
2022-07-21 14:39:28 +0700

    fix: 100%

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -16,7 +17,7 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert(
+        User::insert(
             [
                 [
                     'name'      => 'Admin 1',
