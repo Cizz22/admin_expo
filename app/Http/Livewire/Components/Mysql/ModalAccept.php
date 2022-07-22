c412fd4c9ef60dd1e2771617ad61b2b662779826
Cizz22 <cisatraa@gmail.com>
2022-07-22 21:49:10 +0700

    fix tiket

--- app/Http/Livewire/Components/Mysql/ModalAccept.php
+++ app/Http/Livewire/Components/Mysql/ModalAccept.php
@@ -44,7 +44,8 @@ class ModalAccept extends ModalComponent
         ]);
 
         for ($i = 1; $i <= $this->booking->ticket_total; $i++) {
-            $uniqueId = $this->makeid($total + $i);
+            $total += $i;
+            $uniqueId = $this->makeid($total);
             $barcode = "https://bwipjs-api.metafloor.com/?bcid=code128&text=$uniqueId&scale=3";
             $password = substr(str_shuffle(str_repeat($x = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil(5 / strlen($x)))), 1, 5);
             $name = $this->booking->name;
