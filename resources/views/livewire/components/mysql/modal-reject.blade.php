aec4a19c7d52f4958383d616f701a15f0ad6d49d
Cizz22 <cisatraa@gmail.com>
2022-07-24 18:28:11 +0700

    fix modal

--- resources/views/livewire/components/mysql/modal-reject.blade.php
+++ resources/views/livewire/components/mysql/modal-reject.blade.php
@@ -1,3 +1,22 @@
-<div>
-    {{-- Do your work, then step back. --}}
+<div class="p-4">
+    <div class="flex justify-between">
+        <h5 class="text-lg font-weight-bold">Tolak & <b>Hapus</b> Pembayaran Tiket</h5>
+        <button type="button" title="Tutup" wire:click="closeModal()" class="self-start"><i
+                class="cil-x"></i></button>
+    </div>
+    <hr/>
+    <form wire:submit.prevent="submit" class="mt-4">
+        <p>Apakah anda ingin menolak & <b>mengapus</b>  pembayaran ini?</p>
+        <hr class="my-4"/>
+        <div class="flex flex-wrap mb-2 justify-end">
+            <button wire:loading.remove class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full" type="submit">
+                Ya
+            </button>
+            <div wire:loading>
+
+                <p class="text-xl">Harap Tunggu</p>
+
+            </div>
+        </div>
+    </form>
 </div>
